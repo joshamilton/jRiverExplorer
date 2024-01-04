e8a9fa60b62904bf20a67e6827ddd8292a163d57
Joshua J Hamilton <jhamilton@federation.bio>
2024-01-03 16:48:12 -0800

    Initialize with README, renv, and empty R Shiny app

---
+++ app.R
@@ -0,0 +1,16 @@
+################################################################################
+### app.R
+### Copyright (c) Joshua Hamilton, 2024
+### Email: joshamilton@gmail.com
+################################################################################
+
+library(shiny)
+
+ui = fluidPage(
+
+)
+
+server = function(input, output) {
+}
+
+shinyApp(ui, server)
