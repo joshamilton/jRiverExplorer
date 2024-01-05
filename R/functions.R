87a7feb61d2ff5d306e2b26e1aaeb8b362aabbac
Joshua J Hamilton <jhamilton@federation.bio>
2024-01-05 14:21:35 -0800

    Finish functions to convert XML to dataframe.

--- R/functions.R
+++ R/functions.R
@@ -9,41 +9,41 @@ library(xml2)
 
 # Read in a JRiver Media Center Library file
 # Convert from XML to dataframe
-# Each track is stored as an "Item" with numerous fields, and not all tracks
-# contain the same fields.
-# Convert into a dataframe with the following fields retained as columns:
-#   Genre
-#   Composer
-#   Work
-#   Orchestra
-#   Year Recorded
-# Every track should have these five fields
-# At some point, may want different fields. So the function will read all fields
-# and filter once the dataframe has been constructed.
+# Each track is stored as an "Item" with numerous Fields, and not all tracks
+# contain the same fields. Allow user to specify which fields to retain
 
-# Read the XML file and find all Items and Fields
-xml_file = read_xml('test/library.xml')
-items = xml_find_all(xml_file, "//Item")
-all_field_names = unique(xml_find_all(xml_file, "//Item/Field") %>% xml_attr("Name"))
-
-# Create a dataframe
-tag_df = as.data.frame(matrix(nrow = length(items), ncol = length(all_field_names)))
-colnames(tag_df) = all_field_names
-
-# Function to extract values for each item
-extract_tags <- function(item) {
-  cur_fields <- xml_children(item)
-  cur_field_names <- xml_attr(cur_fields, "Name")
-  cur_values <- xml_text(cur_fields)
+# Extract fields and values for each item
+extract_tags = function(item) {
+  cur_fields = xml_children(item)
+  cur_field_names = xml_attr(cur_fields, "Name")
+  cur_values = xml_text(cur_fields)
   setNames(cur_values, cur_field_names)
 }
 
-# Create dataframe
-tag_df = dplyr::bind_rows(lapply(items, extract_tags))
+# Convert XML file to dataframe
+xml_to_dataframe = function(file) {
+
+  # Read the XML file and find all Items and Fields
+  xml_file = read_xml(file)
+  items = xml_find_all(xml_file, "//Item")
+  all_field_names = unique(xml_find_all(xml_file, "//Item/Field") %>% xml_attr("Name"))
+
+  # Create a dataframe
+  tag_df = as.data.frame(matrix(nrow = length(items), ncol = length(all_field_names)))
+  colnames(tag_df) = all_field_names
+  tag_df = dplyr::bind_rows(lapply(items, extract_tags))
+
+}
 
 # Filter dataframe and simplify to contain unique entries
-columns_to_retain = c('Genre', 'Composer', 'Work', 'Orchestra', 'Year Recorded')
-tag_df = tag_df %>%
-  dplyr::select(all_of(columns_to_retain)) %>%
-  dplyr::distinct()
+filter_dataframe = function(dataframe, columns_to_retain) {
+  tag_df = dataframe %>%
+    dplyr::select(all_of(columns_to_retain)) %>%
+    dplyr::distinct()
+}
 
+# Test code
+file = 'test/library.xml'
+columns_to_retain = c('Genre', 'Composer', 'Work', 'Orchestra', 'Year Recorded')
+tag_df = xml_to_dataframe(file)
+tag_df = filter_dataframe(tag_df, columns_to_retain)
